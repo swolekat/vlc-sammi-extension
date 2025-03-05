ae65e98a3a65a4880269f3865e9687ea0d911015
swolekat <swolecattwitch@gmail.com>
2025-03-05 17:06:47 -0600

    ready for first release

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# VLC Song Title Widget
+# VLC SAMMI Plugin
 
+## Setup
+To use this plugin:
+* Enable the VLC web interface
+* Install the plugin
+* Put your password in the `VLC Variables` button
+* Restart Sammi
 
-## Enabling the VLC Web Interface
+### Enabling the VLC Web Interface
 * Open vlc and open the `Tools` menu then select `Preferences`.
 * Then toggle `All` settings. 
 * Find `Main Interfaces` and check the `Web` checkbox.
@@ -11,3 +17,53 @@
 * Then restart VLC 
 
 If you get a firewall warning, allow.
+
+### Install the Plugin
+* In Sammi open the `Sammi Bridge` Menu
+* `Install an Extension`
+* Locate the sef file (make sure you get it from the release page)
+
+### Setup the Plugin
+The plugin requires the VLC password to communicate with VLC.
+
+If you installed the extension you should see a new `VLC Sammi Extension` deck.
+* Open the deck
+* Open the `VLC Variables` button
+* put your password in the value section of the first line. By default it is `test`. Make sure it's surrounded by double quotes.
+
+After setup, restart Sammi.
+
+## Variables
+In general, the `vlcVariables` button will have all the variables you would want.
+* `title`
+* `artist`
+* `filename`
+* `state` This will be one of `playing`, `paused`, `stopped`
+* `length` The length of the current song in seconds.
+* `time` The time that the current song has been playing for in seconds.
+
+## Triggers
+There are several triggers you can use for when vlc changes
+* `VLC Status Updated` this fires every time the plugin gets a new status. Only use this if you want to show the time being updated. It has the `status` in full if you need it.
+* `VLC Song Changed` this fires when the song changes. This will return all relevant metadata. 
+* `VLC State Changed` this fires when the state of the player changes. This will return `state`. 
+
+## Commands 
+I've tried to cover the basic commands. If you'd like to see more please contact me.
+* `VLC Play`
+* `VLC Pause`
+* `VLC Toggle Pause`
+* `VLC Stop`
+* `VLC Next`
+* `VLC Previous`
+* `VLC Random`
+
+## Lower Third Demo
+To show off what you can do, I've included a "Lower Third" like widget. This will show the new song title whenever one comes on.
+
+* First get your websocket password. It's in OBS under `Tools` and `Websocket Server Settings`
+* Go in the VLC Deck and go to the commands of the `VLC DEMO SETUP` button. Replace `yourWebsocketPasswordGoesHere` with your actual password.
+* Run the button. It'll create the `VLC DEMO LOWER THIRD` scene. 
+* Next go into the `VLC DEMO` button and enable all the commands. 
+
+You should see it work when it changes songs. Feel free to delete the demo buttons or use them as inspiration for your own widget.
--- sammi.js
+++ sammi.js
@@ -1,14 +1,104 @@
 function vlcMain() {
-    const password = 'test';
-    const headers = {
-        "Authorization": `Basic ${btoa(`:${password}`)}`,
-        "Content-Type": "application/json"
+    const VARIABLE_BUTTON_ID = 'vlcVariables';
+    let headers;
+    let currentStatus;
+
+    const startPollLoop = () => {
+        setInterval(() => {
+            SAMMI.httpRequest("http://localhost:8080/requests/status.json", "GET", headers).then(response => {
+                const data = JSON.parse(response.Value);
+                const newStatus = JSON.parse(data.response);
+                // handle first loop
+                if(!currentStatus){
+                    currentStatus = newStatus;
+                    SAMMI.setVariable('time', newStatus.time, VARIABLE_BUTTON_ID)
+                    SAMMI.setVariable('title', title, VARIABLE_BUTTON_ID)
+                    SAMMI.setVariable('artist', artist, VARIABLE_BUTTON_ID)
+                    SAMMI.setVariable('filename', filename, VARIABLE_BUTTON_ID)
+                    SAMMI.setVariable('length', newStatus.length, VARIABLE_BUTTON_ID)
+                    SAMMI.setVariable('state', newStatus.state, VARIABLE_BUTTON_ID)
+                    return;
+                }
+                SAMMI.setVariable('time', newStatus.time, VARIABLE_BUTTON_ID)
+                if(JSON.stringify(newStatus?.information?.category?.meta) !== JSON.stringify(currentStatus?.information?.category?.meta)){
+                    const {artist, title, filename} = newStatus.information.category.meta;
+                    SAMMI.setVariable('title', title, VARIABLE_BUTTON_ID)
+                    SAMMI.setVariable('artist', artist, VARIABLE_BUTTON_ID)
+                    SAMMI.setVariable('filename', filename, VARIABLE_BUTTON_ID)
+                    SAMMI.setVariable('length', newStatus.length, VARIABLE_BUTTON_ID);
+                    SAMMI.triggerExt('VLC Song Changed', newStatus.information.category.meta);
+                }
+                if(currentStatus.state !== newStatus.state){
+                    SAMMI.setVariable('state', newStatus.state, VARIABLE_BUTTON_ID)
+                    SAMMI.triggerExt('VLC State Changed', {state: newStatus.state});
+                }
+                SAMMI.triggerExt('VLC Status Updated', {status: newStatus});
+                currentStatus = newStatus;
+            });
+        }, 1000);
+    };
+
+    const tryToGetPassword = () => {
+        SAMMI.getVariable('password', VARIABLE_BUTTON_ID).then((data) => {
+            if(!data?.Value){
+                SAMMI.alert('VLC Plugin: Could NOT get password from VLC Variables button. Please set it.');
+                setTimeout(tryToGetPassword, 30000);
+            }
+            const password = data.Value;
+            headers = {
+                "Authorization": `Basic ${btoa(`:${password}`)}`,
+                "Content-Type": "application/json"
+            };
+            SAMMI.alert('VLC Plugin: Connected.');
+            startPollLoop();
+        }).catch(() => {
+            SAMMI.alert('VLC Plugin: Could NOT get password from VLC Variables button. Please set it.');
+            setTimeout(tryToGetPassword, 30000);
+        });
     };
 
 
-    SAMMI.httpRequest("http://localhost:8080/requests/status.json", "GET", headers).then(response => {
-        const data = JSON.parse(response.Value);
-        const realData = JSON.parse(data.response);
-        console.log(data)
+    sammiclient.on('VLC Play', () => {
+        SAMMI.httpRequest("http://localhost:8080/requests/status.json", "POST", headers, {
+            command: 'pl_forceresume'
+        })
     });
+
+    sammiclient.on('VLC Pause', () => {
+        SAMMI.httpRequest("http://localhost:8080/requests/status.json", "POST", headers, {
+            command: 'pl_forcepause'
+        })
+    });
+
+    sammiclient.on('VLC Toggle Pause', () => {
+        SAMMI.httpRequest("http://localhost:8080/requests/status.json", "POST", headers, {
+            command: 'pl_pause'
+        })
+    });
+
+    sammiclient.on('VLC Stop', () => {
+        SAMMI.httpRequest("http://localhost:8080/requests/status.json", "POST", headers, {
+            command: 'pl_stop'
+        })
+    });
+
+    sammiclient.on('VLC Next', () => {
+        SAMMI.httpRequest("http://localhost:8080/requests/status.json", "POST", headers, {
+            command: 'pl_next'
+        })
+    });
+
+    sammiclient.on('VLC Previous', () => {
+        SAMMI.httpRequest("http://localhost:8080/requests/status.json", "POST", headers, {
+            command: 'pl_previous'
+        })
+    });
+
+    sammiclient.on('VLC Random', () => {
+        SAMMI.httpRequest("http://localhost:8080/requests/status.json", "POST", headers, {
+            command: 'pl_random'
+        })
+    });
+
+    tryToGetPassword();
 }
